443c2e2b25ab173eb6b4205c4193ebb8daf960a1
Vitali Moiseenok <moiseenokv@gmail.com>
2019-10-20 23:06:42 +0300

    feat: add touch and mmouse events

--- js/index.js
+++ js/index.js
@@ -3,13 +3,11 @@ const eduContent = document.querySelector('.education__content');
 
 const slideLeft = document.querySelector('.projects__slider__nav__prev');  
 const slideRight = document.querySelector('.projects__slider__nav__next');
+const slider = document.querySelector('.projects__slider');
 
+let offsetTouchBegin, offsetTouchEnd;
 
-
-
-let currentSlide;
-
-const slideLeftListner = (event)=>{
+const slideActionLeft = () => {
     event.preventDefault();
     const slides = document.querySelector('.project__slider__items');
     const curSlide = document.querySelector('.project__slider__item.project__slider__item--show');
@@ -25,11 +23,9 @@ const slideLeftListner = (event)=>{
             curSlide.classList.remove('project__slider__item--show');
             slides.children[slidesCount-1].classList.add('project__slider__item--show');
     }
-
 }
 
-const slideRightListner = (event)=>{
-    event.preventDefault();
+const slideActionRight = () => {
     const slides = document.querySelector('.project__slider__items');
     const curSlide = document.querySelector('.project__slider__item.project__slider__item--show');
     const curSlideNumber = parseInt(curSlide.getAttribute('data-slide'));
@@ -43,13 +39,48 @@ const slideRightListner = (event)=>{
     }
 }
 
+const slideLeftListner = (event)=>{
+    event.preventDefault();
+    slideActionLeft();
+
+}
+
+const slideRightListner = (event)=>{
+    event.preventDefault();
+    slideActionRight();
+}
+
 const eduButtonListner = () => {
     eduContent.classList.toggle('education__content--hide');
 }
 
+const handleTouchStartListner = (event) =>{
+    var touch = event.touches[0] || event.changedTouches[0];
+    offsetTouchBegin = touch.pageX
+    console.log('start', touch.pageX);
+}
+
+const handleTouchMoveListner = (event) =>{
+    var touch = event.touches[0] || event.changedTouches[0];
+    offsetTouchEnd = touch.pageX
+}
+
+const handleTouchEndListner = () =>{
+    if((offsetTouchEnd-offsetTouchBegin)>0){
+        slideActionLeft();
+    }
+
+    if((offsetTouchEnd-offsetTouchBegin)<0){
+        slideActionRight();
+    }
+}
 
 
 eduButton.addEventListener('click', eduButtonListner);
 
 slideLeft.addEventListener('click', slideLeftListner);
 slideRight.addEventListener('click', slideRightListner);
+
+slider.addEventListener('touchstart', handleTouchStartListner, false);        
+slider.addEventListener('touchmove', handleTouchMoveListner, false);
+slider.addEventListener('touchend', handleTouchEndListner, false); 
